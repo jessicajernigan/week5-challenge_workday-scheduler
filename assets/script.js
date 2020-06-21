0bb832130505c36046efbc759ce5be76c0272153
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-06-21 15:10:17 -0500

    acceptance criteria achieved; needs some last minute polish

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,11 @@
-
-
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// 1. Add div that displays current date according to user's timezone.
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// 2. Hardcode 8am - 5pm?
-
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// 3. Hour that reflects current time is highlighted using '.present' class; past using '.past'; future using '.future' class
-// WHEN I click into a time block
-// THEN I can enter an event
-// 4. Text field
-
 var displayDate = function () {
-  var prettyDate = moment().format("[Today is] dddd, MMMM Do YYYY, Ha");
+  var prettyDate = moment().format("[Today is] dddd, MMMM Do YYYY, ha");
   moment(prettyDate).toString;
   // console.log(prettyDate);
   var todayDiv = $("#currentDay");
   todayDiv.text(prettyDate);
 };
 
- 
 var colorChange = function () {
   $(".hour").each(function (_index, element) { 
     var calendarHrString = $(this).attr('id');
@@ -45,64 +27,30 @@ var colorChange = function () {
 };
 
 
-// WHEN I click the save button for that time block
-// 5. Save commits change to local storage using hr ID as key, text content as text.
-// 6. Bonus: prompt user when they have not saved?
-
-
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
-
-
-
-
-// var loadTasks = function () {
-//   tasks = JSON.parse(localStorage.getItem("tasks"));
-
-//   // if nothing in localStorage, create a new object to track all task status arrays
-//   if (!tasks) {
-//     tasks = {
-//       toDo: [],
-//       inProgress: [],
-//       inReview: [],
-//       done: []
-//     };
-//   }
-
-//   // loop over object properties
-//   $.each(tasks, function (list, arr) {
-//     // console.log(list, arr); // The purpose of this is to make sure the JavaScript worked when it was inherited.
-//     // then loop over sub-array
-//     arr.forEach(function (task) {
-//       createTask(task.text, task.date, list);
-//     });
-//   });
-// };
-
-// var saveTasks = function () {
-//   localStorage.setItem("tasks", JSON.stringify(tasks));
-// };
-
-// // Task description was clicked.
-// $(".list-group").on("click", "p", function () {
-//   var text = $(this)
-//     .text()
-//     .trim();
-
-//   var textInput = $("<textarea>")
-//     .addClass("form-control")
-//     .val(text);
-
-//   $(this).replaceWith(textInput);
-
-//   textInput.trigger("focus");
-// });
+var populateCalendar = function () {
+  $("#9").siblings("textarea").val(localStorage.getItem("9"));
+  $("#10").siblings("textarea").val(localStorage.getItem("10"));
+  $("#11").siblings("textarea").val(localStorage.getItem("11"));
+  $("#12").siblings("textarea").val(localStorage.getItem("12"));
+  $("#13").siblings("textarea").val(localStorage.getItem("13"));
+  $("#14").siblings("textarea").val(localStorage.getItem("14"));
+  $("#15").siblings("textarea").val(localStorage.getItem("15"));
+  $("#16").siblings("textarea").val(localStorage.getItem("16"));
+  $("#17").siblings("textarea").val(localStorage.getItem("17"));
+};
 
 
 
+$(".saveBtn").on("click", function(event) {
+  event.preventDefault();
+  var eventText = $(this).siblings("textarea").val();
+  var eventHr = $(this).siblings(".time-block").attr("id");
+  console.log(eventText, eventHr);
+
+  localStorage.setItem(eventHr, eventText);
+});
 
 // RUN FUNCTIONS
 displayDate();
 colorChange();
+populateCalendar();
